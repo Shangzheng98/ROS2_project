70002088b03bc7f924f420ce8690f273141aa2f0
Shangzheng Ji <jishangzheng@gmail.com>
2022-03-21 02:15:24 -0400

    changed

--- src/auto_aim/src/auto_aim_node.cpp
+++ src/auto_aim/src/auto_aim_node.cpp
@@ -1,15 +1,25 @@
 #include <cstdio>
 #include "auto_aim/auto_aim.hpp"
+// #include "rcutils/error_handling.h"
+// #include "rclcpp/logger.hpp"
+void signal_handle(int sig){ // can be called asynchronously
+  rclcpp::shutdown(); // set flag
+}
+
 int main(int argc, char ** argv)
 {
   cv::VideoCapture v(0);
   rclcpp::init(argc,argv);
   rclcpp::Node::SharedPtr nh_ = rclcpp::Node::make_shared("auto_aim");
+  
   ArmorDetector armorDetector(nh_);
   cv::Mat frame;
+  signal(SIGINT,signal_handle);
+  //RCLCPP_ERROR(rclcpp::get_logger(), "Error setting severity: %s", rcutils_get_error_string().str);
   while (true)
   {
-    signal();
+    RCLCPP_INFO_ONCE(nh_->get_logger(), "Timer callback called (this will only log once)");
+    
     v.read(frame);
     //armorDetector.armorTask(frame);
     cv::imshow("Live", frame);
@@ -17,4 +27,7 @@ int main(int argc, char ** argv)
   }
   
   return 0;
+
 }
+
+
